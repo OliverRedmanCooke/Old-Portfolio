9a259b49ac1f8c10c3a7bf7a32fd3f40cee0a7ac
Oliver Cooke <oliver.r.cooke@gmail.com>
2019-02-15 11:48:36 +0000

    First build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
 
@@ -35,7 +34,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+<h1> Portfolio </h1> 
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
--- src/pages/blog/2019-02-14-dsnakdjsa.md
+++
@@ -1,9 +0,0 @@
----
-templateKey: blog-post
-title: dsnakdjsa
-date: 2019-02-14T19:28:25.328Z
-description: ndsnajkdnkjsa
-tags:
-  - dsad
----
-jkdsabjdksa
